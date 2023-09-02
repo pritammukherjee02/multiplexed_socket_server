1aa97b43e4925739159bd7e84c8b1bdb479fb48a
pritammukherjee02 <you@example.com>
2023-09-02 06:17:39 +0530

    good ol' console logging way of debugging

--- socket/send.go
+++ socket/send.go
@@ -1,8 +1,12 @@
 package socket
 
-import "net"
+import (
+	"net"
+)
 
 func SendData(data []byte, conn net.Conn) error {
+	loggers.DEBUG("About to send data")
 	_, err := conn.Write(data)
+	loggers.DEBUG("Sent")
 	return err
 }
