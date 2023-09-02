7e7c76d41efdaa846829bda20981c990df24bdd8
pritammukherjee02 <you@example.com>
2023-09-02 17:23:25 +0530

    trying to log err info if any while writing to con

--- socket/send.go
+++ socket/send.go
@@ -7,6 +7,9 @@ import (
 func SendData(data []byte, conn net.Conn) error {
 	loggers.DEBUG("About to send data")
 	_, err := conn.Write(data)
+	if err != nil {
+		loggers.ERR("Encountered error while writing to the client connection: " + err.Error())
+	}
 	loggers.DEBUG("Sent")
 	return err
 }
